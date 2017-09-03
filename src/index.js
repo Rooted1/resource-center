0ab8926b6f78c8a5c5ca056f933a946d2e0d9ad7
Rooted1 <ruthobey@gmail.com>
2017-09-02 23:08:00 -0500

    change primary color to pink

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#ff80aa'
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
